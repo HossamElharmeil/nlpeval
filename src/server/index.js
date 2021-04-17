2600b45dad863dc11ba1443c7f2866ac45b607ca
HossamElharmeil <hossam.elharmil@gmail.com>
2021-04-17 03:42:00 +0200

    README: initial draft

--- src/server/index.js
+++ src/server/index.js
@@ -1,22 +1,47 @@
 var path = require('path')
+const dotenv = require('dotenv');
+const FormData = require('form-data')
+const fetch = require('node-fetch')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
 
+dotenv.config();
+
 const app = express()
+const PORT = process.env.PORT || 8081
 
 app.use(express.static('dist'))
 
-console.log(__dirname)
+app.use(express.json())
+
+app.listen(PORT, function () {
+    console.log(`Example app listening on port ${PORT}!`)
+})
 
 app.get('/', function (req, res) {
     res.sendFile('dist/index.html')
 })
 
-// designates what port the app will listen to for incoming requests
-app.listen(8080, function () {
-    console.log('Example app listening on port 8080!')
-})
 
 app.get('/test', function (req, res) {
     res.send(mockAPIResponse)
+})
+
+app.post('/analyze', (req, res) => {
+    var data = new FormData()
+    data.append('url', req.body.url)
+    data.append('key', process.env.API_KEY)
+    data.append('lang', "auto")
+
+    fetch("https://api.meaningcloud.com/sentiment-2.1", {
+        method: 'post',
+        body: data
+    })
+        .then(result => {
+            res.json(result)
+        })
+        .catch(error => {
+            console.error(error);
+            res.status(500).json({error: "Something went wrong"})
+        })
 })
