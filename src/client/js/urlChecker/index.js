dc99598efa76756131ff53a07a80986590bd88cf
HossamElharmeil <hossam.elharmil@gmail.com>
2021-04-21 06:20:25 +0200

    urlChecker: regex credits

--- src/client/js/urlChecker/index.js
+++ src/client/js/urlChecker/index.js
@@ -1,4 +1,5 @@
 function checkForUrl(inputUrl) {
+    //Regex from stack overflow https://stackoverflow.com/questions/3809401/
     const regex = new RegExp(/[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/gi)
     
     if (inputUrl.match(regex)) {
